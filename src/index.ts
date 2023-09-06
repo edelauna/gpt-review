5e80fdb9df20183abf19193b8ffc3281979e036e
Elliott de Launay <edelauna@gmail.com>
2023-09-06 21:13:51 +0000

    feat(git-diff): throwing custom error class on missing base ref

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import * as core from '@actions/core';
 import * as gitDiff from './git-diff';
 import * as makeReview from './openai'
 
-async function run() {
+export const run = async () => {
   try {
     const diffs = await gitDiff.run()
     await makeReview.run(diffs);
   } catch (error) {
-    core.setFailed((error as Error).message);
+    switch((error as Error).name) {
+      case 'GitDiffBaseRefException':
+        core.warning((error as Error).message);
+        break;
+      default:
+        core.setFailed((error as Error).message);
+        break;
+    }
   }
 }
 
