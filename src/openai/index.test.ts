d48ad6b97d0d75bbf5475ac293a18f27ab5335aa
Elliott de Launay <edelauna@gmail.com>
2023-09-08 13:24:59 +0000

    feat(openai): adding ability to specify files, or the start of a file part to be ignored

--- src/openai/index.test.ts
+++ src/openai/index.test.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import { run } from ".";
 import { Diff } from "../git-diff";
 import { makeReview, roomForReview } from "./utils/make-review";
@@ -10,12 +11,21 @@ jest.mock('./utils/make-review', () => ({
   makeReview: jest.fn(),
   roomForReview: jest.fn()
 }));
+jest.mock('@actions/core', () => ({
+  getInput: jest.fn(),
+  info: jest.fn()
+}))
+
+beforeEach(() => jest.resetAllMocks())
 
 test('run() call messageManager and makeReview for each diff', async () => {
   const input: Diff[] = [
     { fileName: 'file1.diff', patch: [] },
     { fileName: 'file2.diff', patch: [] },
   ];
+  const inputMock = core.getInput as jest.Mock
+  inputMock.mockReturnValue("")
+
   const messageManagerMock = messageManager as jest.Mock
   messageManagerMock.mockReturnValueOnce([
     { role: 'system', content: 'System message' },
@@ -34,5 +44,35 @@ test('run() call messageManager and makeReview for each diff', async () => {
   await run(input);
 
   expect(messageManagerMock).toHaveBeenCalledTimes(2);
+  expect(makeReviewMock).toHaveBeenCalledTimes(1);
+});
+
+test('run() ignores files', async () => {
+  const input: Diff[] = [
+    { fileName: 'file1.diff', patch: [] },
+    { fileName: 'file2.diff', patch: [] },
+    { fileName: 'target/file2.diff', patch: [] },
+  ];
+  const inputMock = core.getInput as jest.Mock
+  inputMock.mockReturnValue("file2.diff,target")
+
+  const messageManagerMock = messageManager as jest.Mock
+  messageManagerMock.mockReturnValueOnce([
+    { role: 'system', content: 'System message' },
+    { role: 'user', content: 'User message 1' }
+  ])
+    .mockReturnValueOnce([
+      { role: 'system', content: 'System message' },
+      { role: 'user', content: 'User message 1' },
+      { role: 'user', content: 'User message 2' }
+    ]);
+
+  const roomForReviewMock = roomForReview as jest.Mock
+  roomForReviewMock.mockReturnValue(true);
+
+  const makeReviewMock = makeReview as jest.Mock
+  await run(input);
+
+  expect(messageManagerMock).toHaveBeenCalledTimes(1);
   expect(makeReviewMock).toHaveBeenCalledTimes(1);
 });
--- src/openai/index.ts
+++ src/openai/index.ts
@@ -1,9 +1,23 @@
+import * as core from '@actions/core';
 import { type Diff } from "../git-diff";
 import { makeReview, roomForReview } from "./utils/make-review";
 import { messageManager } from "./utils/message-manager";
 
+const ignoreFiles = () => core.getInput("IGNORE_FILES").split(",").filter(f => f != '');
+
+const filesToBeIgnored = (file: string) =>{
+  const ignore = ignoreFiles().filter(f => file.startsWith(f));
+  const predicate = ignore.length > 0
+  if(predicate){
+    core.info(`Skipping analysis of patch, ${file} matched the following: ${ignore}`);
+  }
+  return predicate
+}
+
 export const run = async (input: Diff[]) => {
   for (const diff of input) {
+    if(filesToBeIgnored(diff.fileName)) continue;
+
     const messages = messageManager(diff);
     if(!roomForReview()) break;
 
